fbd8429252db9b07447ae395a885820ab28d2e76
Abdellah <ramadanabdel24@gmail.com>
2024-08-26 15:09:49 +0100

    add licence information

--- src/Api/AbstractApiController.php
+++ src/Api/AbstractApiController.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Cameroon Open Data project.
+ *
+ * (c) Abdellah (Abdel) Ramadan
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Api;
 
 use App\Service\YamlParserServiceInterface;
